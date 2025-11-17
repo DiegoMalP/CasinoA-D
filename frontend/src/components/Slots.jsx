123fa7e405f5f2d27341d7e1fc0439553fdfd06b
Diego Maldonado <Maldonadodiego544@gmail.com>
2025-11-17 22:50:41 +0100

    final

--- frontend/src/components/Slots.jsx
+++ frontend/src/components/Slots.jsx
@@ -1,7 +1,5 @@
 import { useRef, useEffect, useState } from "react";
 
-import '../styles/slots.css'
-
 export default function TragaPremios() {
     const canvasRef = useRef(null);
     const [msg, setMsg] = useState("¡Haz click para jugar!");
